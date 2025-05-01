74294fcce904cc98658063c55f96d1801e623654
leonardhan-ujet <leonard.han@ujet.cx>
2025-05-01 15:19:54 +0900

    - react-router, react-query 패키지 추가 및 적용 - pages 디렉토리 생성

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(<App />);
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+    </QueryClientProvider>
+);
