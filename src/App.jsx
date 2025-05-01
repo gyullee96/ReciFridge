06426e1d959314cf443a615fd662c38f89b82d98
leonard <hans8114@gmail.com>
2025-05-01 17:40:11 +0900

    - zustand추가 - Route 구조 대충 잡기 - env 추가 - 공공데이터 api 호출 추가 - NaviBar 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AppLayout from './layout/AppLayout';
 import Home from './pages/HomePage/Home';
+import RecipeDetail from './pages/RecipeDetailPage/RecipeDetail';
 
 function App() {
-  console.log('Running');
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+      <Route path="/"> {/* todo : cover page */}
+        <Route path="home" element={<Home />} />
+        <Route path="recipe" element={<AppLayout />}>
+          <Route index element={<RecipeDetail />} /> {/* todo : recipe page */}
+          <Route path=":id" element={<RecipeDetail />} />
+        </Route>
+      </Route>
+    </Routes >
   );
 }
 
