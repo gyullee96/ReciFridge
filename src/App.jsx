86e088a1594ce14604d29761ff1169300bc1f526
gyullee96 <gyullee96@naver.com>
2025-05-02 21:53:29 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
 import { Route, Routes } from 'react-router-dom';
+import React from 'react';
 import './App.css';
 import Home from './pages/HomePage/Home';
+import CoverPage from './pages/CoverPage/CoverPage';
 
 function App() {
   console.log('Running');
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<CoverPage />} />
+      <Route path="/main" element={<Home />} />
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import App from './App.jsx';
 import './index.css';
 
@@ -8,10 +9,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <App />
-            <ReactQueryDevtools initialIsOpen={false} />
-        </BrowserRouter>
-    </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
