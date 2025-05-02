f2dd9addfc128b0d69e58c7b58f52c60c84e0295
leonard <hans8114@gmail.com>
2025-05-02 20:29:31 +0900

    add new recipe details

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,26 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Button from '@mui/material/Button';
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-const AppLayout = () => {
+const AppLayout = ({ title }) => {
+    const navigate = useNavigate();
+
     return (
         <div>
-            AppLayout
+            <div style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: '#fffaef' }}>
+                <Button
+                    variant="contained"
+                    color="dark"
+                    startIcon=<ArrowBackIosIcon />
+                    onClick={() => navigate(-1)}
+                ></Button>
+                <div style={{ display: 'flex', justifyContent: 'center', width: '100%', textAlign: 'center' }} >
+                    Test
+                </div>
+            </div>
             <Outlet />
-        </div>
+        </div >
     )
 }
 
--- src/pages/RecipeDetailPage/RecipeDetail.jsx
+++ src/pages/RecipeDetailPage/RecipeDetail.jsx
@@ -2,20 +2,44 @@ import CircularProgress from '@mui/material/CircularProgress';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useRecipeDetail from '../../hooks/useRecipeDetail';
+import './RecipeDetail.style.css';
 
 const RecipeDetail = () => {
     const { id } = useParams();
     const { data, isLoading, isError, error } = useRecipeDetail({ id, keyword: 'test', page: 1, filter: null });
     const [recipe, setRecipe] = useState(null);
     const [code, setCode] = useState(null);
+    const [manual, setManual] = useState([]);
+    const [ingredient, setIngredient] = useState([]);
+
     console.log('RecipeDetail');
 
     console.log(id, data, code, recipe);
 
+    const FILED_COUNT = 20;
+
     useEffect(() => {
-        console.log('updated', code, recipe);
-        setRecipe(data?.data?.COOKRCP01?.row[0]);
-        setCode(data?.data?.COOKRCP01?.RESULT?.CODE);
+        let recipe = data?.data?.COOKRCP01?.row[0];
+        if (recipe) {
+            setRecipe(recipe);
+            setCode(data.data.COOKRCP01.RESULT.CODE);
+
+            let manual = [];
+            for (let i = 1; i <= FILED_COUNT; ++i) {
+                const numString = i < 10 ? `0${i}` : `${i}`;
+                const manualName = `MANUAL${numString}`;
+                const manualImgName = `MANUAL_IMG${numString}`;
+
+                if (recipe[manualName] === '')
+                    break;
+
+                manual.push([recipe[manualImgName], recipe[manualName]]);
+            }
+            setManual(manual);
+
+            let ingredient = recipe.RCP_PARTS_DTLS.split('●');
+            setIngredient(ingredient);
+        }
     }, [data]);
 
     if (isLoading || !recipe) {
@@ -31,12 +55,30 @@ const RecipeDetail = () => {
     }
 
     return (
-        <div>
-            <div>
-                RecipeDetail {id}
+        <div className='recipe-main'>
+            <div style={{ marginTop: 10 }}>
+                <img src={recipe.ATT_FILE_NO_MAIN} className='image-box' />
             </div>
-            <div>
+            <h1 style={{ fontWeight: 800 }}>
                 {recipe.RCP_NM}
+            </h1>
+            <p style={{ width: '80%' }}>
+                {recipe.RCP_NA_TIP}
+            </p>
+            <hr style={{ width: '100%', border: '3px solid #A1C8C4' }}></hr>
+            <h3 style={{ width: '80%' }}>
+                {ingredient.map((part, index) => {
+                    return (<div key={index} style={{ margin: '10px' }}>{part}</div>)
+                })}
+            </h3>
+            <hr style={{ width: '100%', border: '3px solid #A1C8C4' }}></hr>
+            <div className='description-container'>
+                {manual.map((content, index) => {
+                    return (<div key={index}>
+                        <img src={content[0]} className='image-box' />
+                        <div>{content[1]}</div>
+                    </div>)
+                })}
             </div>
         </div>
     )
