b056a6a627f59d8ed994bbafa67db759caef9249
coldme <radiantbeautykim@gmail.com>
2025-05-04 00:14:39 +0900

    config sever 내용 삭제

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://openapi.foodsafetykorea.go.kr',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/7109179c29414b97985b'), // API key 포함
-        secure: false,
-      },
-    },
-  },
 });
