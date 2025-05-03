f98de470673bdf588d479e80250a9f7b6cba7249
gyullee96 <gyullee96@naver.com>
2025-05-04 00:10:04 +0900

    second commit

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -17,53 +17,62 @@ const Home = () => {
   };
 
   return (
-    <div className="refridge-border">
-      <div className="main-top">
-        <div>
-          <img src="/fridge-main.png" alt="" className="refridge-main" />
+    <div className="page-container">
+      <div className="refridge-border">
+        <div className="main-top">
+          <div>
+            <img src="/fridge-main.png" alt="" className="refridge-main" />
+          </div>
+          <div className="main-logo">ReciFridge</div>
         </div>
-        <div className="main-logo">ReciFridge</div>
-      </div>
-      <div className="ingredient" onClick={() => navigate('/status')}>
-        <div>My items: 18</div>
-        <ul>
-          {ingredients.map((item) => (
-            <li key={item.id}>
-              ID: {item.id}, 유통기한: {item.expiration}, 개수: {item.count}
-            </li>
-            // 재료 아이콘 나오면 쓸 코드
-            // <li key={item.id}>
-            //   {item.icon}
-            // </li>
-          ))}
-        </ul>
-      </div>
-      <div className="expiration">
-        <div>소비기한 임박</div>
-        <ul>
-          {ingredients.map((item) => {
-            const dDay = calculateDday(item.expiration);
-            let dDayText = '';
+        <div className="content">
+          <div className="ingredient" onClick={() => navigate('/status')}>
+            <div>
+              My items:
+              {ingredients
+                ? ingredients.reduce((total, item) => total + item.count, 0)
+                : 0}
+            </div>
+            <ul>
+              {ingredients.map((item) => (
+                <li key={item.id}>
+                  ID: {item.id}, 유통기한: {item.expiration}, 개수: {item.count}
+                </li>
+                // 재료 아이콘 나오면 쓸 코드
+                // <li key={item.id}>
+                //   {item.icon}
+                // </li>
+              ))}
+            </ul>
+          </div>
+          <div className="expiration">
+            <div>소비기한 임박</div>
+            <ul>
+              {ingredients.map((item) => {
+                const dDay = calculateDday(item.expiration);
+                let dDayText = '';
 
-            if (dDay > 0) {
-              dDayText = `D-${dDay}`;
-            } else if (dDay === 0) {
-              dDayText = 'Today!';
-            } else {
-              dDayText = `D+${Math.abs(dDay)}`;
-            }
+                if (dDay > 0) {
+                  dDayText = `D-${dDay}`;
+                } else if (dDay === 0) {
+                  dDayText = 'Today!';
+                } else {
+                  dDayText = `D+${Math.abs(dDay)}`;
+                }
 
-            return (
-              <li key={item.id}>
-                아이콘자리 {item.count} {dDayText}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+                return (
+                  <li key={item.id}>
+                    아이콘자리 {item.count} {dDayText}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
 
-      <div className="main-bottom">
-        <NavFooter />
+        <div className="main-bottom">
+          <NavFooter />
+        </div>
       </div>
     </div>
   );
